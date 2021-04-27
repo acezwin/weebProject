092e4266e0a636f740c789e843c240f0e174aaf7
Quinn Howard <77995884+kiwamiquinn@users.noreply.github.com>
2021-04-26 21:51:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -24,15 +24,16 @@ function updateDom(data){
             return `
             </div>
                 <div class="card">
-                    <div class="card-body">
-                        <img src="${anime.image_url}" width ="300" height="400">
-                    </div>
                     <div class="card-content">
                         <span class="card-title">${anime.title}</span>
                         <p>${anime.synopsis}</p>
                     </div>
+                    <div class="card-body">
+                        <img src="${anime.image_url}" width ="160" height="240">
+                    </div>
                     <div class="card-action">
                         <a href="${anime.url}">Find out more</a>
+                        <button class="btn btn-info float-right btn-sm" onclick="addToCart('+item['id']+')">Add to Watchlist</button>
                     </div>
                 </div>
             `
@@ -58,6 +59,49 @@ function toShoppingCart(){
     } else {
         alert("Please enter your email at top of page."); //alert user since email is empty
     }
+}
+
+function addToCart($id) {
+    let email =$.trim($('#email').val()); //gets the user's email
+    
+    if (email != ''){
+    sessionStorage.setItem("email", email);
+    $.ajax({
+        url: Url + 'AddToCart',
+        type: 'post',
+        dataType: 'json',
+        data: JSON.stringify({"anime.title": $id, "email" : email}),
+        contentType: 'text/plain',
+
+        success: function (data) { //on success
+            //fetchOneProduct($id);
+           
+            //sessionStorage.productListAdd($id);
+            alert("All done"); 
+
+        }, 
+        error: function (data){
+            alert("Wrong"); 
+        }
+    });  
+    }
+    else {
+        alert("something wrong here in else");
+        
+    }      
+}
+
+document.getElementById("btnDarkmode").onclick = function Darkmode(){
+    //console.log("dark button working")
+    document.body.style.backgroundColor = "rgb(32, 33, 36)";
+    //document.getElementsByClassName(".card").style.backgroundColor = "rgb(55,57,59)";
+    
+}
+document.getElementById("btnLightmode").onclick = function Lightmode(){
+    //console.log("light button working")
+    document.body.style.backgroundColor = "rgb(255, 255, 255)";
+    //document.getElementsByClassName(".card").style.backgroundColor = "rgb(55,57,59)";
+    
 }
 
 $('#exampleModal').on('show.bs.modal', function (event) {
