4682b3cece8b5b82c4ede12238c477005aeafd30
Gingerhouse <abelvillape@gmail.com>
2021-05-05 16:41:16 -0400

    Finished Manga Filter

--- app.js
+++ app.js
@@ -7,11 +7,60 @@ function searchAnime(event){
 
     const form = new FormData(this);
     const query = form.get("search");
+    var genre = document.getElementById("genre")
+    const genreChecked = '';
 
-    fetch(`${base_url}/search/anime?q=${query}&page=1`)
+    for(i = 0; i < 43; i++){
+        if(genre[i].checked){
+            genreChecked = '&genre='+genre[i];
+        }
+    }
+    
+    fetch(`${base_url}/search/anime?q=${query}&page=1${genreChecked}&genre_exclude=0`)
     .then(res=>res.json())
     .then(updateDom)
     .catch(err=>console.warn(err.message));
+}
+
+function advancedAnime() {
+    var checkBox = document.getElementById("advancedCheck");
+    var text = document.getElementById("advancedAnime");
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+}
+
+function searchManga(event){
+
+    event.preventDefault();
+
+    const form = new FormData(this);
+    const query = form.get("search");
+    var genre = document.getElementById("genre")
+    const genreChecked = '';
+
+    for(i = 0; i < 45; i++){
+        if(genre[i].checked){
+            genreChecked = genreChecked + '&genre='+genre[i];
+        }
+    }
+
+    fetch(`${base_url}/search/manga?q=${query}&page=1${genreChecked}&genre_exclude=0`)
+    .then(res=>res.json())
+    .then(updateDom)
+    .catch(err=>console.warn(err.message));
+}
+
+function advancedManga() {
+    var checkBox = document.getElementById("advancedCheck");
+    var text = document.getElementById("advancedManga");
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
 }
 
 function updateDom(data){
